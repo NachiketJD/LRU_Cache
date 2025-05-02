0fb189fa17afd64f9ab0ffdd5263df6827da06b4
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-02 22:21:45 +0530

    commit 7

--- main.go
+++ main.go
@@ -38,3 +38,35 @@ func (l *LRUCache) Get(key int) int {
 	}
 	return -1
 }
+
+
+func main() {
+	// Test case 1
+	// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+	// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+	//	[null, null, null, 1, null, -1, null, -1, 3, 4]
+	fmt.Println("Test case 1")
+	obj := Constructor(2)
+	obj.Put(1, 1)
+	obj.Put(2, 2)
+	fmt.Println(obj.Get(1))
+	obj.Put(3, 3)
+	fmt.Println(obj.Get(2))
+	obj.Put(4, 4)
+	fmt.Println(obj.Get(1))
+	fmt.Println(obj.Get(3))
+	fmt.Println(obj.Get(4))
+
+	// Test case 2
+	// 	["LRUCache","put","put","put","put","get","get"]
+	// [[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
+	// [null,null,null,null,null,-1,3]
+	fmt.Println("Test case 2")
+	obj = Constructor(2)
+	obj.Put(2, 1)
+	obj.Put(1, 1)
+	obj.Put(2, 3)
+	obj.Put(4, 1)
+	fmt.Println(obj.Get(1))
+	fmt.Println(obj.Get(2))
+}
