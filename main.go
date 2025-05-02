68fe30547ef074a662aee3b233b571542f246ba7
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-02 22:20:53 +0530

    commit 5

--- main.go
+++ main.go
@@ -16,3 +16,18 @@ type LRUCache struct {
 unc Constructor(capacity int) LRUCache {
 	return LRUCache{Queue: list.New(), Items: make(map[int]*Node), Capacity: capacity}
 }
+
+func (l *LRUCache) Put(key int, value int) {
+	if item, ok := l.Items[key]; !ok {
+		if l.Capacity == len(l.Items) {
+			back := l.Queue.Back()
+			l.Queue.Remove(back)
+			delete(l.Items, back.Value.(int))
+		}
+		l.Items[key] = &Node{Data: value, KeyPtr: l.Queue.PushFront(key)}
+	} else {
+		item.Data = value
+		l.Items[key] = item
+		l.Queue.MoveToFront(item.KeyPtr)
+	}
+}
